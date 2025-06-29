11bb22fe3545659a4e147c01416a8897bb959f50
ABDELLAHI <abdellahielwavi21@gmail.com>
2025-06-29 20:50:59 +0000

    Remplacer le service twilio par un service locale

---
+++ src/main/java/com/cas/sur/tout/urgents/dto/otpVerification/ValidationResponse.java
@@ -0,0 +1,11 @@
+package com.cas.sur.tout.urgents.dto.otpVerification;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ValidationResponse {
+    private int code;
+    private double balance;
+}
